bc8de063f3420099f1918d949012c71eee01208a
Rafael Ghossi <rafael.ghossi@gmail.com>
2015-11-13 02:47:55 -0500

    Finish Him! by thais

--- popup/popup-data.js
+++ popup/popup-data.js
@@ -8,9 +8,16 @@ $(function () {
 	function() {	
 		bName = document.getElementById("bName").value;
 		bDescrip = document.getElementById("bDescrip").value;
-		bColor = $('input[name="bColort"]:checked').val();
+		bColor = $('input[name="bColor"]:checked').val();
 		bBookend = document.getElementById("bBookend").value;
 		
 		console.log(bName + "-" + bDescrip + "-" + bColor + "-" + bBookend);
+
+		$("#shelf").append("<span class='"+ bColor + "'>" + bName + "</span>");
+		
+		
+		$("#eBookend_overlay").fadeOut(200);
+		$("#signup").css({"display":"none"})
+		
 	});
  });
